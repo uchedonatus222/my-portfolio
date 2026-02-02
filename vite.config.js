16632291186f4858162d029e5e40f3b576a3ab28
donatusuche222 <donatusuche222@gmail.com>
2026-02-02 14:43:34 +0100

    Fix: Configure Tailwind CSS correctly

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,5 +7,5 @@ export default defineConfig({
     port: 3000,
     strictPort: true, // Recommended: prevents Vite from switching to 3001 if 3000 is busy
   },
-  plugins: [react(),tailwindcss()],
+  plugins: [react()],
 })
